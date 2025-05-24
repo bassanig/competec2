b1509509906ea776498e553cba810aefa475238a
bassanig <guilherme.bassani07@gmail.com>
2025-05-24 12:04:06 -0300

    Update Main.java

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+        Scanner leitor = new Scanner(System.in);
+        
+        int tamanhoTriangulo, tamanhoLinha;
+        
+        System.out.print("Digite o numero de asteriscos do seu triangulo: ");
+        tamanhoTriangulo = leitor.nextInt();
+        tamanhoLinha = tamanhoTriangulo;
+        
+        for (int i = 1; i <= tamanhoTriangulo; i++) {
+            for (int y = 1; y <= tamanhoLinha; y++) {
+                System.out.print('*');
+            }
+            System.out.println("");
+            tamanhoLinha--;
+        }
 	}
 }
